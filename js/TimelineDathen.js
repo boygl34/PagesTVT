3d3698c8a19f9fdf4aec2a263a5fa0846737a551
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-11-15 20:54:09 +0700

    add
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- js/TimelineDathen.js
+++ js/TimelineDathen.js
@@ -56,6 +56,7 @@ var options = {
   editable: true,
   autoResize: true,
   zoomable: false,
+  moveable: false,
   onAdd: function (item, callback) {
     $("#CoVanDichVu").html("");
     getData(urlTX);
