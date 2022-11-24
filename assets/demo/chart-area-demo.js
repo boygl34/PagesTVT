72977cfbbea9701678f42017749c77e35d47e37f
boygl34 <56708291+boygl34@users.noreply.github.com>
2022-11-24 15:25:16 +0700

    add
    Signed-off-by: boygl34 <56708291+boygl34@users.noreply.github.com>

--- assets/demo/chart-area-demo.js
+++ assets/demo/chart-area-demo.js
@@ -2,68 +2,64 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-var url = "https://script.google.com/macros/s/AKfycbxCM-a6aevPEt4gmv4YYS7ziOja6yYf0XRYwq8KPbnP-DXruv17Iy6iQmgf5BanHEzh/exec"
+var url = "https://fresh-shimmering-sorrel.glitch.me/BaoCao/11-2022"
 console.log("truc")
 var data1
-$.get(url, function(ketqua) {
- 
- data1=ketqua.data.BaoCaoHen
+$.get(url, function (ketqua) {
+  console.log(ketqua.data.BaoCaoTiep[0].map(function (r) { return r }))
 
-console.log(data1[0])
-
-
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: data1[0],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: data1[1],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-         // max: auto,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
+  // Area Chart Example
+  var ctx = document.getElementById("myAreaChart");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ketqua.data.BaoCaoTiep[0].map(function (r) { return r }),
+      datasets: [{
+        label: "Gặp LT",
+        lineTension: 0.3,
+        backgroundColor: "rgba(2,117,216,0.2)",
+        borderColor: "rgba(2,117,216,1)",
+        pointRadius: 7,
+        pointBackgroundColor: "rgba(2,117,216,1)",
+        pointBorderColor: "rgba(255,255,255,0.8)",
+        pointHoverRadius: 7,
+        pointHoverBackgroundColor: "rgba(2,117,216,1)",
+        pointHitRadius: 50,
+        pointBorderWidth: 2,
+        data: ketqua.data.BaoCaoTiep[2].map(function (r) { return r }),
       }],
     },
-    legend: {
-      display: false
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 10
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            //max: auto,
+            //maxTicksLimit: 5
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, .125)",
+          }
+        }],
+      },
+      legend: {
+        display: false
+      }
     }
-  }
+  });
+
+
+
+
 });
-
-
-
-
-} );
